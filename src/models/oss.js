ab80c72b0c82f51091d5e622cfb4902e5af9c84c
zhaobb4 <zhaobb4@lenovo.com>
2022-07-04 14:29:28 +0800

    fix: git ignore

--- src/models/oss.js
+++ src/models/oss.js
@@ -3,209 +3,203 @@
  key生成规则：用户id/保存类别_当前时间戳.图片类型
  let key = this.props.global.currentUser._id + '/photo_' + new Date().getTime() + '.' + file.name.split('.')[1];
  */
-import { notification } from 'antd'
-import { ENV, Storage, Request, FetchGet } from '@/utils'
+import { notification } from "antd";
+import { ENV, Storage, Request, FetchGet } from "@/utils";
 
 const getClient = function (data) {
   return new window.OSS.Wrapper({
+    endpoint: data.endpoint,
     region: data.region,
     bucket: data.bucket,
     accessKeyId: data.credentials.AccessKeyId,
     accessKeySecret: data.credentials.AccessKeySecret,
     stsToken: data.credentials.SecurityToken
-  })
-}
+  });
+};
 //base64转Blob
 const toBlob = function (urlData, fileType) {
   let bytes = window.atob(urlData),
     n = bytes.length,
-    u8arr = new Uint8Array(n)
+    u8arr = new Uint8Array(n);
   while (n--) {
-    u8arr[n] = bytes.charCodeAt(n)
+    u8arr[n] = bytes.charCodeAt(n);
   }
-  return new Blob([u8arr], {type: fileType})
-}
+  return new Blob([u8arr], { type: fileType });
+};
 
 export default {
-  namespace: 'oss',
+  namespace: "oss",
 
   state: {
     loading: false,
     submitting: false,
-    exif: ''
+    exif: "",
   },
 
   effects: {
-
     // 列表
-    *list({payload, callback}, {call, put}) {
+    *list({ payload, callback }, { call, put }) {
       yield put({
-        type: 'changeLoading',
+        type: "changeLoading",
         payload: true,
-      })
+      });
       const res = yield call(
         (params) => {
-          return Request('/api/PhotoList', {method: 'POST', body: params,})
+          return Request("/api/PhotoList", { method: "POST", body: params });
         },
-        {payload}
-      )
-      yield callback(res)
+        { payload }
+      );
+      yield callback(res);
       yield put({
-        type: 'changeList',
+        type: "changeList",
         payload: res,
-      })
+      });
       yield put({
-        type: 'changeLoading',
+        type: "changeLoading",
         payload: false,
-      })
+      });
     },
 
     // 详情
-    *detail({payload, callback}, {call, put}) {
+    *detail({ payload, callback }, { call, put }) {
       yield put({
-        type: 'changeLoading',
+        type: "changeLoading",
         payload: true,
-      })
-      const res = yield call(
-        (params) => {
-          return Request('/api/FileOssEdit', {method: 'POST', body: params,})
-        },
-        {}
-      )
-      yield callback(res)
+      });
+      const res = yield call((params) => {
+        return Request("/api/FileOssEdit", { method: "POST", body: params });
+      }, {});
+      yield callback(res);
       yield put({
-        type: 'changeDetail',
+        type: "changeDetail",
         payload: res,
-      })
+      });
       yield put({
-        type: 'changeLoading',
+        type: "changeLoading",
         payload: false,
-      })
+      });
     },
 
     // 源数据
-    *exif({payload, callback}, {call, put}) {
+    *exif({ payload, callback }, { call, put }) {
+      let url = payload.url + "?x-oss-process=image/info";
 
-      let url = payload.url + '?x-oss-process=image/info';
-
-      const exif = yield FetchGet(url)
+      const exif = yield FetchGet(url);
 
       yield callback(exif);
-
     },
 
     // 上传
-    *upload({payload, callback}, {call, put}) {
-      let res,
-        ossToken = Storage.get(ENV.storage.ossToken, 7200)
-      if(ossToken){
-        res = ossToken
+    *upload({ payload, callback }, { call, put }) {
+      let res;
+      const ossToken = Storage.get(ENV.storage.ossToken, 3600);
+      if (ossToken) {
+        res = ossToken;
       } else {
-        res = yield call(
-          (params) => {return Request('/oss/token', {method: 'POST', body: params})},
-          {}
-        )
-        Storage.set(ENV.storage.ossToken, res)
+        res = yield call((params) => {
+          return Request("/oss/token", { method: "POST", body: params });
+        }, {});
+        Storage.set(ENV.storage.ossToken, res);
       }
-
       if (res.code === 0) {
-        let client = getClient(res.data)
-        client.multipartUpload(payload.key, payload.file, {})
+        let client = getClient(res.data);
+        client
+          .multipartUpload(payload.key, payload.file, {})
           .then(function (r) {
-            let url = r.res.requestUrls[0].split('?')[0]
-            url = url.replace('metuwang.oss-cn-qingdao.aliyuncs.com', 'photo.metuwang.com')
-            return callback(url)
+            let url = r.res.requestUrls[0].split("?")[0];
+            url = url.replace(
+              "metuwang.oss-cn-qingdao.aliyuncs.com",
+              "photo.metuwang.com"
+            );
+            return callback(url);
           })
           .catch(function (err) {
             notification.error({
-              message: '上传失败！',
+              message: "上传失败！",
               //description: err,
-            })
-          })
+            });
+          });
       } else {
         notification.error({
           message: res.message,
-        })
+        });
       }
     },
 
-    *uploadBase64({payload, callback}, {call, put}) {
-      const res = yield call(
-        (params) => {return Request('/api/FileOssEdit', {method: 'POST', body: params})},
-        {}
-      )
+    *uploadBase64({ payload, callback }, { call, put }) {
+      const res = yield call((params) => {
+        return Request("/api/FileOssEdit", { method: "POST", body: params });
+      }, {});
       if (res.code === 0) {
+        let client = getClient(res.data);
 
-        let client = getClient(res.data)
+        let dataUrl = payload.base64;
+        let base64 = dataUrl.split(",")[1];
+        let fileType = dataUrl.split(";")[0].split(":")[1];
+        let blob = toBlob(base64, fileType);
+        let reader = new FileReader();
 
-        let dataUrl = payload.base64
-        let base64 = dataUrl.split(',')[1]
-        let fileType = dataUrl.split(';')[0].split(':')[1]
-        let blob = toBlob(base64, fileType)
-        let reader = new FileReader()
-
-        reader.readAsArrayBuffer(blob)
+        reader.readAsArrayBuffer(blob);
         reader.onload = function (event) {
           // arrayBuffer转Buffer
-          let buffer = new window.OSS.Buffer(event.target.result)
-          client.put(payload.key, buffer).then(function (r) {
-            let url = r.res.requestUrls[0].split('?')[0]
-            url = url.replace('metuwang.oss-cn-qingdao.aliyuncs.com', 'photo.metuwang.com')
-            return callback(url)
-          }).catch(function (err) {
-            notification.error({
-              message: '上传失败！',
-              description: err,
+          let buffer = new window.OSS.Buffer(event.target.result);
+          client
+            .put(payload.key, buffer)
+            .then(function (r) {
+              let url = r.res.requestUrls[0].split("?")[0];
+              url = url.replace(
+                "metuwang.oss-cn-qingdao.aliyuncs.com",
+                "photo.metuwang.com"
+              );
+              return callback(url);
             })
-          })
-        }
+            .catch(function (err) {
+              notification.error({
+                message: "上传失败！",
+                description: err,
+              });
+            });
+        };
       }
-
     },
 
-    *del({payload, callback}, {call, put}) {
+    *del({ payload, callback }, { call, put }) {
       yield put({
-        type: 'changeSubmitting',
+        type: "changeSubmitting",
         payload: true,
-      })
-      const res = yield call(
-        (params) => {
-          return Request('/api/FileOssEdit', {method: 'POST', body: params,})
-        },
-        {}
-      )
+      });
+      const res = yield call((params) => {
+        return Request("/api/FileOssEdit", { method: "POST", body: params });
+      }, {});
       if (res.code === 0) {
-        let client = getClient(res.data)
-        client.deleteMulti(payload.keys)
-          .catch(function (err) {
-            notification.error({
-              message: '删除失败！',
-              description: err,
-            })
-          })
+        let client = getClient(res.data);
+        client.deleteMulti(payload.keys).catch(function (err) {
+          notification.error({
+            message: "删除失败！",
+            description: err,
+          });
+        });
       }
       yield put({
-        type: 'changeSubmitting',
+        type: "changeSubmitting",
         payload: false,
-      })
+      });
     },
-
   },
 
   reducers: {
-    changeLoading(state, {payload}) {
+    changeLoading(state, { payload }) {
       return {
         ...state,
         loading: payload,
-        uploadAvatar: '',
-      }
+        uploadAvatar: "",
+      };
     },
-    changeSubmitting(state, {payload}) {
+    changeSubmitting(state, { payload }) {
       return {
         ...state,
         submitting: payload,
-      }
+      };
     },
-  }
-
-}
+  },
+};
